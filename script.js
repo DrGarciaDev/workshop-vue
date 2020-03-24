24881754472f973d63374482ac4b8d4089226839
Luis García <luis_grdz@hotmail.com>
2020-03-23 18:57:42 -0600

    Video 4: agregar elementos a uan lista; v-if v-else @click metodos

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 new Vue({
     el: '#appVue',
     data: {
-        lists: [
-            {keep: 'uno', completed: true},
-            {keep: 'dos', completed: false},
-        ],
+        lists: [],
+        newKeep: ''
+    },
+    methods: {
+        addKeep: function(){
+            this.lists.push({keep: this.newKeep, completed: false});
+            this.newKeep = '';
+        }
     }
 });
